2f406cc45963d776db1ae817c2438def791f0336
alpgul <5773704+alpgul@users.noreply.github.com>
2026-09-06 20:21:58 +0300

    build(curl): switch to lexiforest curl-chrome impersonate fork
    - clone lexiforest/curl-chrome (impersonate-chrome branch) instead of
      ading2210/curl 8.17.0-patched
    - link against legion/boringssl (--with-openssl) instead of mbedtls
    - enable ECH (--enable-ech) for TLS grease support
    - pass ac_cv_func_pipe2=no to configure instead of sed-editing
      configure.ac (the upstream if/then/fi block would be left empty
      and the generated configure would break)
    - install the built libcurl-impersonate.a as libcurl.a

--- client/tools/curl.sh
+++ client/tools/curl.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-#compile openssl for use with emscripten
+#compile curl-chrome (lexiforest curl-impersonate fork) against the
+#boringssl impersonate fork for use with emscripten
 
 set -x
 set -e
 
 CORE_COUNT=$(nproc --all)
 PREFIX=$(realpath build/curl-wasm)
-MBEDTLS_PREFIX=$(realpath build/mbedtls-wasm)
+BORINGSSL_PREFIX=$(realpath build/boringssl-wasm)
 ZLIB_PREFIX=$(realpath build/zlib-wasm)
 BROTLI_PREFIX=$(realpath build/brotli-wasm)
+ZSTD_PREFIX=$(realpath build/zstd-wasm)
 NGHTTP2_PREFIX=$(realpath build/nghttp2-wasm)
 
 cd build
 rm -rf curl
-git clone -b 8.17.0-patched --depth=1 https://github.com/ading2210/curl
+git clone -b impersonate-chrome --depth=1 https://github.com/lexiforest/curl-chrome curl
 cd curl
 
-#emscripten does not support the pipe2 syscall
-sed -i '/pipe2/d' configure.ac
-
+#emscripten does not support the pipe2 syscall; force-configure it off via
+#the autoconf cache variable instead of editing configure.ac (the upstream
+#if/then/fi block would be left empty and generated configure breaks)
 autoreconf -fi
 emconfigure ./configure --host i686-linux \
+  ac_cv_func_pipe2=no \
   --disable-shared --disable-threaded-resolver --without-libpsl \
   --disable-netrc --disable-ipv6 --disable-tftp --disable-ntlm-wb \
   --enable-websockets --disable-ftp --disable-file --disable-gopher \
   --disable-imap --disable-mqtt --disable-pop3 --disable-rtsp \
   --disable-smb --disable-smtp --disable-telnet --disable-dict \
-  --with-mbedtls=$MBEDTLS_PREFIX --with-zlib=$ZLIB_PREFIX \
-  --with-brotli=$BROTLI_PREFIX --with-nghttp2=$NGHTTP2_PREFIX
+  --with-openssl=$BORINGSSL_PREFIX --with-zlib=$ZLIB_PREFIX \
+  --with-brotli=$BROTLI_PREFIX --with-zstd=$ZSTD_PREFIX \
+  --with-nghttp2=$NGHTTP2_PREFIX --enable-ech
 
 emmake make -j$CORE_COUNT CFLAGS="-O3" LIBS="-lbrotlicommon"
 
@@ -36,6 +40,6 @@ rm -rf $PREFIX
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 cp -r include/curl $PREFIX/include
-cp lib/.libs/libcurl.a $PREFIX/lib
+cp lib/.libs/libcurl-impersonate.a $PREFIX/lib/libcurl.a
 
 cd ../../
